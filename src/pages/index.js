084e239a5497059f5e996397e7bab10e314c4a43
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-06 14:56:36 +0000

    added contact us form component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,18 @@ import TitlePage from "../components/TitlePage";
 import LocalizedLink from "../components/LocalizedLink";
 import useTranslations from "../components/useTranslations";
 import useMenu from "../components/useMenu";
+import Form from 'customisable-contact-form'
+import {
+    Heading,
+    FirstName,
+    LastName,
+    Email,
+    Message,
+    SubmitButton
+} from 'customisable-contact-form'
 
 import * as S from "../components/ListWrapper/styled";
+import { Contact } from "styled-icons/boxicons-solid";
 
 const Index = ({ data: { allMarkdownRemark } }) => {
     // useTranslations is aware of the global context (and therefore also "locale")
@@ -31,30 +41,58 @@ const Index = ({ data: { allMarkdownRemark } }) => {
             <TitlePage text={services} id={sectionId("services")} />
             <div className="card-container">
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{width: "100%"}}/>
+                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{ width: "100%" }} />
                     <div className="container">
                         <h4><b>John Doe</b></h4>
                         <p>Architect & Engineer</p>
                     </div>
                 </div>
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{width: "100%"}}/>
+                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{ width: "100%" }} />
                     <div className="container">
                         <h4><b>John Doe</b></h4>
                         <p>Architect & Engineer</p>
                     </div>
                 </div>
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{width: "100%"}}/>
+                    <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{ width: "100%" }} />
                     <div className="container">
                         <h4><b>John Doe</b></h4>
                         <p>Architect & Engineer</p>
                     </div>
                 </div>
             </div>
+            <hr style={{ margin: `2rem 0` }} />
+
+            <br />
+
+            <ContactUs />
+            
         </div>
     );
 };
+
+function ContactUs() {
+    const theme = {
+    primaryFont: "Bookman",
+    inputBorderRadius: "13px",
+    inputBorderWeight: "1px",
+    width: "70%"
+    }
+    const title = "Contact Us"
+    return (
+        <>
+             <Form theme={theme}>
+                <Heading title={title} />
+                <FirstName />
+                <LastName />
+                <Email />
+                <Message />
+                <SubmitButton />
+            </Form>
+        </>
+    )
+}
 
 export default Index;
 
