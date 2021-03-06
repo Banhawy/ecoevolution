b9a9f6256ac7877e569c3b0d2289287d1b7e2b74
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-06 14:28:47 +0000

    added services header

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import * as S from "../components/ListWrapper/styled";
 const Index = ({ data: { allMarkdownRemark } }) => {
     // useTranslations is aware of the global context (and therefore also "locale")
     // so it'll automatically give back the right translations
-    const { hello, subline, mission, missionText } = useTranslations();
+    const { hello, subline, mission, services, missionText } = useTranslations();
     const useMenuItems = useMenu();
     const sectionId = (section) =>
         useMenuItems.filter((menuItem) => menuItem.name === section).link;
@@ -28,6 +28,7 @@ const Index = ({ data: { allMarkdownRemark } }) => {
             <hr style={{ margin: `2rem 0` }} />
 
             <br />
+            <TitlePage text={services} id={sectionId("services")} />
             <div className="card-container">
                 <div className="card">
                     <img src="https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Avatar" style={{width: "100%"}}/>
