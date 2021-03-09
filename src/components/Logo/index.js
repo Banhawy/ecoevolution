d025c834a6f473e7ff45998aa3fed60efdd80fd4
banhawy <the.benhawy@gmail.com>
2021-03-09 20:56:16 +0200

    replaced logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from '../../images/logo-iceberg.svg';
+import logo from '../../images/ecoEvolutionLogo.png';
 
 const Logo = () => {
-  return <img src={logo} alt="Iceberg Logo" />;
+  return <img src={logo} alt="EcoEvolution Logo" />;
 };
 
 export default Logo;
