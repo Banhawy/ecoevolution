a79803b3b6bdcd9980e57f1e12569f71cb89805f
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-10 18:24:06 +0200

    mdbreact headers

--- src/components/TitlePage/index.js
+++ src/components/TitlePage/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { MDBTypography } from 'mdbreact'
 import * as S from './styled';
 
 const TitlePage = props => {
   const { text } = props;
 
-  return <S.TitleElement>{text}</S.TitleElement>;
+  return <S.TitleElement><MDBTypography className="text-center titles" tag="h2" variant="display-2">{text}</MDBTypography></S.TitleElement>
 };
 
 export default TitlePage;
