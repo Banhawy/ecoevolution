f14a876a6d15a5771b8d39f5d1fb0663fb48b493
Adham El Banhawy <the.benhawy@gmail.com>
2021-03-13 18:44:11 +0200

    Styled & animated card

--- src/components/HeroCard/index.js
+++ src/components/HeroCard/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { MDBTypography, MDBIcon } from 'mdbreact';
+import { MDBTypography, MDBIcon, MDBView, MDBMask, MDBAnimation } from 'mdbreact';
 
 const HeroCard = () => {
     return (
         <>
             <div className="card-container">
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1560037962-08931d95007f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
-                        alt="Avatar" style={{ width: "100%" }} />
+                    <AnimatedCard />
                 </div>
             </div>
             <div className="card-container">
@@ -26,5 +25,27 @@ const HeroCard = () => {
         </>
     )
 }
+
+const AnimatedCard = () => (
+    <MDBAnimation reveal type="slideInLeft" count={1}>
+        <MDBView hover>
+            <img src="https://images.unsplash.com/photo-1560037962-08931d95007f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+                alt="Avatar" style={{ width: "100%" }} />
+            <MDBMask className="flex-center" overlay="green-light">
+                <ol className="white-text">
+                    <li>
+                        <MDBTypography tag="h3" variant="h2" colorText="white">Bullet points are lame</MDBTypography>
+                    </li>
+                    <li>
+                        <MDBTypography tag="h3" variant="h2" colorText="white">Bullet points are lame</MDBTypography>
+                    </li>
+                    <li>
+                        <MDBTypography tag="h3" variant="h2" colorText="white">Bullet points are lame</MDBTypography>
+                    </li>
+                </ol>
+            </MDBMask>
+        </MDBView>
+    </MDBAnimation>
+)
 
 export default HeroCard;
